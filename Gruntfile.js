53b8adb0add63d79407fe337c1d77fd422c60024
Andy Walpole <me@andywalpole.me>
2015-11-13 20:12:54 +0000

    added load-grunt-tasks and time-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,9 @@
 
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
   // Project configuration.
   grunt.initConfig({
 
@@ -70,12 +73,6 @@ module.exports = function(grunt) {
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-jscs');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
